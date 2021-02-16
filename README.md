5ea9a789474eabd49cbda3c16abff4d0f74dee78
Justin Fernandez <justin.miguel.fernandez@gmail.com>
2021-02-16 00:52:31 -0500

    checkers game completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Checkers AI
+
+Developing different machine learning and artificial intelligence methods to solving the game of chess.
--- checkers/piece.py
+++ checkers/piece.py
@@ -10,13 +10,7 @@ class Piece:
         self.row = row
         self.col = col
         self.color = color
-        self.king = False
-
-        if self.color == RED:
-            self.direction = -1
-        else: #self.color == WHITE
-            self.direciton = 1
-        
+        self.king = False        
         self.x = 0
         self.y = 0
         self.calc_pos()
@@ -34,6 +28,11 @@ class Piece:
         pygame.draw.circle(win,self.color,(self.x,self.y),radius)
         if self.king:
             win.blit(CROWN,(self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
+    
+    def move(self,row,col):
+        self.row = row
+        self.col = col
+        self.calc_pos()
 
     def __repr__(self):
         return str(self.color)
