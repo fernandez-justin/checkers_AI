449fb3ab2518da31371e63ab362e27a3519dce58
Justin Fernandez <justin.miguel.fernandez@gmail.com>
2021-02-23 17:12:33 -0500

    updated rest of game for AI

--- checkers/game.py
+++ checkers/game.py
@@ -1,29 +1,27 @@
 import pygame
-from .constants import RED,WHITE,GREEN,SQUARE_SIZE
+from .constants import RED, WHITE, GREEN, SQUARE_SIZE
 from checkers.board import Board
 
 class Game:
-    def __init__(self,win):
+    def __init__(self, win):
         self._init()
         self.win = win
-
+    
     def update(self):
         self.board.draw(self.win)
         self.draw_valid_moves(self.valid_moves)
         pygame.display.update()
 
     def _init(self):
-        '''Init helper that will be called in real init'''
         self.selected = None
         self.board = Board()
         self.turn = RED
         self.valid_moves = {}
 
     def winner(self):
-        self.board.winner()
+        return self.board.winner()
 
     def reset(self):
-        '''Basically an init method but used to reset after play'''
         self._init()
 
     def select(self, row, col):
@@ -41,22 +39,23 @@ class Game:
             
         return False
 
-    def _move(self,row,col):
-        piece = self.board.get_piece(row,col)
-        if self.selected and piece == 0 and (row,col) in self.valid_moves:
-            self.board.move(self.selected,row,col)
-            skipped = self.valid_moves[(row,col)]
+    def _move(self, row, col):
+        piece = self.board.get_piece(row, col)
+        if self.selected and piece == 0 and (row, col) in self.valid_moves:
+            self.board.move(self.selected, row, col)
+            skipped = self.valid_moves[(row, col)]
             if skipped:
                 self.board.remove(skipped)
             self.change_turn()
         else:
             return False
+
         return True
 
-    def draw_valid_moves(self,moves):
+    def draw_valid_moves(self, moves):
         for move in moves:
             row, col = move
-            pygame.draw.circle(self.win,GREEN,(col*SQUARE_SIZE + SQUARE_SIZE//2,row*SQUARE_SIZE + SQUARE_SIZE//2), 15)
+            pygame.draw.circle(self.win, GREEN, (col * SQUARE_SIZE + SQUARE_SIZE//2, row * SQUARE_SIZE + SQUARE_SIZE//2), 15)
 
     def change_turn(self):
         self.valid_moves = {}
@@ -64,3 +63,10 @@ class Game:
             self.turn = WHITE
         else:
             self.turn = RED
+
+    def get_board(self):
+        return self.board
+
+    def ai_move(self, board):
+        self.board = board
+        self.change_turn()
